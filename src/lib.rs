5883095bcc2c3cc33727f68e3f9ac42248d2bc6d
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2025-04-26 02:34:48 +0100

    Add documentation comments for matrix, frame, and utils modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 #![doc = include_str!("../README.md")]
 
+/// Documentation for the [`crate::matrix`] module.
 pub mod matrix;
 
+/// Documentation for the [`crate::frame`] module.
 pub mod frame;
 
+/// Documentation for the [`crate::utils`] module.
 pub mod utils;
