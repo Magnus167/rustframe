127e178b7902d9fe0480021647c647121b92fc2d
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2025-05-15 00:08:58 +0100

    Remove commented-out code and streamline module exports in dateutils

--- src/utils/dateutils/mod.rs
+++ src/utils/dateutils/mod.rs
@@ -1,8 +1,5 @@
 pub mod bdates;
-// pub use bdates::{BDateFreq, BDatesList, BDatesGenerator};
-
 pub mod dates;
-// pub use dates::{DateFreq, DatesList, DatesGenerator};
-// pub mod base;
-// pub use base::{BDatesGenerator, BDatesList};
-// pub use base::{DateFreq, DatesGenerator, DatesList};
+
+pub use bdates::{BDateFreq, BDatesGenerator, BDatesList};
+pub use dates::{DateFreq, DatesGenerator, DatesList};
