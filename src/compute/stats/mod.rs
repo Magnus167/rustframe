b2f6794e05c2cd21e20143221d7c2654dd554b5a
Palash Tyagi <23239946+Magnus167@users.noreply.github.com>
2025-07-15 01:02:20 +0100

    Add inferential module to stats module exports

--- src/compute/stats/mod.rs
+++ src/compute/stats/mod.rs
@@ -1,7 +1,9 @@
 pub mod correlation;
 pub mod descriptive;
 pub mod distributions;
+pub mod inferential;
 
 pub use correlation::*;
 pub use descriptive::*;
 pub use distributions::*;
+pub use inferential::*;
